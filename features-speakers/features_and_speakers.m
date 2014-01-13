bd032048b2f0f1599a152619eb368507e42e3a8f
jaivalis <johnaivalis@gmail.com>
2014-01-13 14:55:32 +0100

    final outputs taahm

--- features-speakers/features_and_speakers.m
+++ features-speakers/features_and_speakers.m
@@ -5,13 +5,13 @@ facedetfname    = sprintf('%09d_%09d_facedets.mat', s1, s2);
 facedets = load(fullfile(result_dir, facedetfname));
 load(fullfile(model_dir, 'mean_face.mat'));
 
-facedets = face_features(facedets, dump_string);
+facedets = face_features(facedets.facedets, dump_string);
 facedets = face_descriptors(facedets, mean_face, 101, dump_string, model_dir);
 
 % we don't need that in our project
 % facedets = mouth_motion(facedets, dump_string);
 % facedets = declare_speakers(facedets);
 
-save(fullfile(result_dir, facedets));
+save(fullfile(result_dir, facedetfname));
 
 end
